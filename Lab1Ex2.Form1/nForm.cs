c754a3f711b0f5683b32985dfe7c23958f16f2f1
jijilak <95184520+jijilak@users.noreply.github.com>
2022-07-10 14:24:59 +0300

    Lab1Ex4

---
+++ Lab1Ex2.Form1/nForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Lab1Ex2.Form1
+{
+    public partial class nForm : Lab1Ex2.Form1.Form1
+    {
+        public nForm()
+        {
+            InitializeComponent();
+        }
+
+        private void nForm_Load(object sender, EventArgs e)
+        {
+            //Application.Run(new nForm());
+        }
+    }
+}
