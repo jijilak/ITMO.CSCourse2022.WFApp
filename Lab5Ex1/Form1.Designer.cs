0f51975852cacb5231da4a861f5416f8f93bdc36
jijilak <95184520+jijilak@users.noreply.github.com>
2022-07-19 00:46:52 +0300

    add

--- Lab5Ex1/Form1.Designer.cs
+++ Lab5Ex1/Form1.Designer.cs
@@ -37,7 +37,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(28, 27);
+            this.textBox1.Location = new System.Drawing.Point(28, 35);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 22);
             this.textBox1.TabIndex = 0;
@@ -47,9 +47,10 @@
             // 
             this.button1.Location = new System.Drawing.Point(28, 84);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(112, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Tag = "Add table...";
+            this.button1.Text = "Add table";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
