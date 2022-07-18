0156fe24483c89825231c13695ebaf7e47ba8a07
jijilak <95184520+jijilak@users.noreply.github.com>
2022-07-18 19:38:31 +0300

    от 18.07.2022

--- Lab2Ex6.BiblWorm/Form1.cs
+++ Lab2Ex6.BiblWorm/Form1.cs
@@ -16,5 +16,89 @@ namespace Lab2Ex6.BiblWorm
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+                public string Author // автор
+        {
+            get { return textBox1.Text; }
+            set { textBox1.Text = value; }
+        }
+        public string Title // Название
+        {
+            get { return textBox2.Text; }
+            set { textBox2.Text = value; }
+        }
+        public string PublishHouse // Издательство
+        {
+            get { return textBox3.Text; }
+            set { textBox3.Text = value; }
+        }
+        public int Page // Количество страниц
+        {
+            get { return (int)numericUpDown1.Value; }
+            set { numericUpDown1.Value = value; }
+        }
+        public int Year // Год издания
+        {
+            get { return (int)numericUpDown2.Value; }
+            set { numericUpDown2.Value = value; }
+        }
+        public int InvNumber // Инвентарный номер
+        {
+            get { return (int)numericUpDown3.Value; }
+            set { numericUpDown3.Value = value; }
+        }
+        public bool Existence // Наличие
+        {
+            get { return checkBox1.Checked; }
+            set { checkBox1.Checked = value; }
+        }
+        public bool SortInvNumber // Сортировка по инвентарному номеру
+        {
+            get { return checkBox2.Checked; }
+            set { checkBox2.Checked = value; }
+        }
+        public bool ReturnTime // Возвращение в срок
+        {
+            get { return checkBox3.Checked; }
+            set { checkBox3.Checked = value; }
+        }
+        public int PeriodUse // Инвентарный номер
+        {
+            get { return (int)numericUpDown4.Value; }
+            set { numericUpDown4.Value = value; }
+        }
+        List<Lab2Ex6.BiblWorm.Item> its = new List<Lab2Ex6.BiblWorm.Item>();
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Lab2Ex6.BiblWorm.Book b = new Lab2Ex6.BiblWorm.Book(Author, Title, PublishHouse, Page, Year, InvNumber, Existence);
+            if (ReturnTime) b.ReturnSrok();
+            b.PriceBook(PeriodUse);
+            its.Add(b);
+            Author = Title = PublishHouse = "";
+            Page = InvNumber = PeriodUse = 1;
+            Year = 2000;
+            Existence = ReturnTime = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            {
+
+                if (SortInvNumber) its.Sort();
+
+                StringBuilder sb = new StringBuilder();
+                foreach (Lab2Ex6.BiblWorm.Item item in its)
+                {
+                    sb.Append("\n" + item.ToString());
+                }
+
+                richTextBox1.Text = sb.ToString();
+            }
+        }
     }
 }
---
+++ Lab2Ex6.BiblWorm/IPubs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab2Ex6.BiblWorm
+{
+    interface IPubs
+    {
+        
+       void Subs ();
+                
+    }
+}
