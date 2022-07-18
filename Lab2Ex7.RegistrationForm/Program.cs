91e579bd3b7571bd562bafaaca5a604a1d85fbc8
jijilak <95184520+jijilak@users.noreply.github.com>
2022-07-18 21:48:01 +0300

    18.07.2022

---
+++ Lab2Ex7.RegistrationForm/Program.cs
@@ -0,0 +1,23 @@
+﻿using Lab2Ex7.RegistrationForm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab2Ex7.RegistrationForm
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
